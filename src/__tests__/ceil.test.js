6a5fd55202b0eae3ac6c3eca6f3b319ed557e29d
Henri Kiiski <henri.kiiski@live.com>
2021-11-22 16:31:15 +0200

    Added non number input tests to ceil.test.js

--- src/__tests__/ceil.test.js
+++ src/__tests__/ceil.test.js
@@ -25,4 +25,39 @@ describe('Ceil', () => {
       expect(ceil(Number.MIN_SAFE_INTEGER - 1.5)).toEqual(Number.MIN_SAFE_INTEGER - 1)
     })
   })
+  describe('Input is a number with precision', () => {
+    it('positive number with positive precision', () => {
+      expect(ceil(5.555, 2)).toEqual(5.56)
+    })
+    it('positive number with negative precision', () => {
+      expect(ceil(555.555, -2)).toEqual(600)
+    })
+    it('negative number with positive precision', () => {
+      expect(ceil(-5.555, 2)).toEqual(-5.55)
+    })
+    it('negative number with negative precision', () => {
+      expect(ceil(-555.555, -2)).toEqual(-500)
+    })
+    it('input is zero', () => {
+      expect(ceil(0.000, 2)).toEqual(0.00)
+    })
+  })
+  describe('Input is not a number', () => {
+    it('input is a string integer', () => {
+      expect(ceil('69.70')).toEqual(70)
+    })
+    it('input is a string number with precision', () => {
+      expect(ceil('34.56', 1)).toEqual(34.6)
+    })
+    it('input is a string but not a number', () => {
+      expect(ceil('this is not a number')).toBeNaN()
+    })
+    it('input is an array', () => {
+      expect(ceil([1, 2, 4])).toBeNaN()
+    })
+    it('input is an object', () => {
+      const obj = {'1': '23'}
+      expect(ceil(obj)).toBeNaN()
+    })
+  })
 });
