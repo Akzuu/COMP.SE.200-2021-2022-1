69c2f97597a8204075ad1795fd0dfd3e7348db23
Akseli Kolari <33838340+Akzuu@users.noreply.github.com>
2021-12-03 10:29:47 +0200

    Happy tests for words (#3)

--- src/__tests__/words.test.js
+++ src/__tests__/words.test.js
@@ -1,8 +1,19 @@
 import words from '../words';
 
-
 describe('words', () => {
-  test('', () => {
-    
-  })
+  it('splits normal string into an array of its words', () => {
+    expect(words('fred, barney & pebbles')).toEqual(['fred', 'barney', 'pebbles'])
+  });
+
+  it('allows using custom regexp as the pattern', () => {
+    expect(words('fred, barney & pebbles', /[^, ]+/g)).toEqual(['fred', 'barney', '&', 'pebbles']);
+  });
+
+  it('handles empty string as an input', () => {
+    expect(words('')).toEqual([]);
+  });
+
+  it('handles pattern which does not match anything', () => {
+    expect(words('Cat walked the line', /[]+g/));
+  });
 });
